9ad214326fdaf39b8cd8a919bead9497e1155f79
jaredfholgate <jared.holgate@hashicorp.com>
2022-01-26 10:02:46 +0000

    Tidy up

--- main.tf
+++ main.tf
@@ -27,40 +27,46 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
+locals {
+  image_name = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+  image_type = "hvm"
+  image_owner = "099720109477" # Canonical
+}
+
+
 provider "aws" {
-    alias = "aws_01"
+    alias = "aws_01" # NOTE: Set the provider alias here!
     region = "us-east-1"
     access_key = var.aws_access_key_01
     secret_key = var.aws_secret_key_01
 }
 
 provider "aws" {
-    alias = "aws_02"
+    alias = "aws_02" # NOTE: Set the provider alias here!
     region = "us-east-1"
     access_key = var.aws_access_key_02
     secret_key = var.aws_secret_key_02
 }
 
 data "aws_ami" "ubuntu_01" {
-    provider = aws.aws_01
+    provider = aws.aws_01 # NOTE: Use the provider alias here!
     most_recent = true
 
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+        values = [local.image_name]
     }
 
     filter {
         name   = "virtualization-type"
-        values = ["hvm"]
+        values = [local.image_type]
     }
 
-    owners = ["099720109477"] # Canonical
+    owners = [local.image_owner]
 }
 
 resource "aws_instance" "ec2_01" {
-  provider = aws.aws_01
+  provider = aws.aws_01 # NOTE: Use the provider alias here!
   ami           = data.aws_ami.ubuntu_01.id
   instance_type = "t3.micro"
 
@@ -70,24 +76,24 @@ resource "aws_instance" "ec2_01" {
 }
 
 data "aws_ami" "ubuntu_02" {
-    provider = aws.aws_01
+    provider = aws.aws_02 # NOTE: Use the provider alias here!
     most_recent = true
 
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+        values = [local.image_name]
     }
 
     filter {
         name   = "virtualization-type"
-        values = ["hvm"]
+        values = [local.image_type]
     }
 
-    owners = ["099720109477"] # Canonical
+    owners = [local.image_owner]
 }
 
 resource "aws_instance" "ec2_02" {
-  provider = aws.aws_02
+  provider = aws.aws_02 # NOTE: Use the provider alias here!
   ami           = data.aws_ami.ubuntu_02.id
   instance_type = "t3.micro"
 
